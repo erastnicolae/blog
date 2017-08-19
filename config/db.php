b4753a1af47cdf7b39585a883ecf385c919e39ee
Nicolae Erast <erast.nicolae@gmail.com>
2017-08-19 23:47:24 +0300

    Change .gitignore, added db schema

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'sqlite:@app/blog.db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
